059b05dd8fd0f8b0849bc4980df0c6c9c292577f
Constanza Gabriela Kuschel Navarrete <48297866+conikuschel@users.noreply.github.com>
2021-11-29 14:15:30 -0300

    mm

--- subpub/serializers.py
+++ subpub/serializers.py
@@ -5,8 +5,7 @@ from subpub.models import Transaccion
 class TransaccionSerializer(serializers.ModelSerializer):
     class Meta:
         model=Transaccion
-        fields=('id', 'tipo', 'banco_origen', 'cuenta_origen', 'banco_destino', 'cuenta_destino', 'monto', 'message_id',
-        'fecha',
+        fields=('id', 'tipo', 'banco_origen', 'cuenta_origen', 'banco_destino', 'cuenta_destino', 'monto', 'message_id','fecha',
         )
 
 
